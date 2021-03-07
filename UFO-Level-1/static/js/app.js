e30a8678bd1e56102d2949d9ad367c96209f9594
JSbarra <justine.m.sbarra@gmail.com>
2021-03-06 21:46:12 -0500

    updated code

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,31 @@
 // from data.js
+
 var tableData = data;
+console.log(tableData)
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody")
+
+for(var i=0; i < tableData.length; i++) {
+    var rows = tbody.append("tr")
+    rows.append('td').text(tableData[i]['datetime'])
+    rows.append('td').text(tableData[i]['city'])
+    rows.append('td').text(tableData[i]['state'])
+    rows.append('td').text(tableData[i]['country'])
+    rows.append('td').text(tableData[i]['shape'])
+    rows.append('td').text(tableData[i]['durationMinutes'])
+    rows.append('td').text(tableData[i]['comments'])
+}
+
+tableData.forEach(function(i){
+    rows = tbody.append('tr')
+    rows.append('td').text(['datetime'])
+    rows.append('td').text(tableData[i]['city'])
+    rows.append('td').text(tableData[i]['state'])
+    rows.append('td').text(tableData[i]['country'])
+    rows.append('td').text(tableData[i]['shape'])
+    rows.append('td').text(tableData[i]['durationMinutes'])
+    rows.append('td').text(tableData[i]['comments'])
+});
+
