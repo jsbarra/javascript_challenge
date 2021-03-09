4b4181cfdc53f5cee696c887d2b87fb6b96bbe0e
JSbarra <justine.m.sbarra@gmail.com>
2021-03-08 22:50:48 -0500

    reupdating code

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -29,3 +29,33 @@ tableData.forEach(function(i){
     rows.append('td').text(tableData[i]['comments'])
 });
 
+d3.select("#filter-btn").on("click", filter_data);
+function filter_data() {
+    var user_data = d3.select("#datetime").property("value")
+    var city_data = d3.select("#city").property("value")
+    var state_data = d3.select("#state").property("value")
+    var country_data = d3.select("#country").property("value")
+    var shape_data = d3.select("#shape").property("value")
+    var date_filter = tableData
+
+    if(user_data != "") {
+        date_filter = date_filter.filter(d => d.datetime == city_data)
+     }
+    
+    if (city_data != "") {
+        date_filter = date_filter.filter(d => d.city == city_data)
+    }
+
+    if(state_data != "") {
+        date_filter = date_filter.filter(d => d.state == state_user)
+    }
+
+    if(country_data != "") {
+        date_filter = date_filter.filter(d => d.country == country_data)
+    }
+
+    if(shape_data != "") {
+        date_filter = date_filter.filter(d => d.shape == shape_data)
+    }
+
+}
